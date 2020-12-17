cfe7fc58a3977e5a51f78be9c4b6cdee829c20f8
Bryan Musungu <bryanmusungu@Bryans-MacBook-Pro.local>
2020-12-16 18:53:21 -0600

    remove old folder

--- DogApp-Proj/src/main/java/com/example/demo/repository/DogPicsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.demo.model.DogPics;
-
-public interface DogPicsRepository extends MongoRepository<DogPics, String> {
-
-}
